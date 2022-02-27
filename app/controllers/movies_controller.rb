1d856817074c5454686192986606952d05147087
EC2 Default User <ec2-user@ip-172-31-12-137.us-east-2.compute.internal>
2022-02-27 05:08:08 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,7 @@ class MoviesController < ApplicationController
 
   def index
     @sort_by = params[:sort_by]
+    @all_ratings = Movie.all_ratings
     
     if @sort_by == 'title'
       @movies = Movie.order(:title)
